e1d44b1dec536864eae419d936fbfe315eb16470
Nickolay <tereshcuk@yandex.ru>
2025-11-16 17:24:47 +0300

    ДЗ_1

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -1,10 +1,25 @@
 const {checkHealthStatus} = require("../index.js");
 
-test("checkHealthStatus", () => {
+test("checkHealthStatus_1", () => {
 const list = {name: "Маг", health: 80};
 const result = checkHealthStatus(list);
-console.log(result);
 expect(result).toBe("healthy");
 
 }  );
+
+test("checkHealthStatus_2", () => {
+const list = {name: "Маг", health: 40};
+const result = checkHealthStatus(list);
+expect(result).toBe("wounded");
+
+}  );
+
+test("checkHealthStatus_3", () => {
+const list = {name: "Маг", health: 4};
+const result = checkHealthStatus(list);
+expect(result).toBe("critical");
+
+}  );
+
+
 
